da93caffeecaf7d1c785a8ee2dad2d3da6c2c667
totodore <prevottheodore@gmail.com>
2026-05-19 09:28:38 +0200

    docs: build with feature flag tags

--- api-error/src/lib.rs
+++ api-error/src/lib.rs
@@ -1,6 +1,7 @@
 // Copyright 2025-Present Centreon
 // SPDX-License-Identifier: Apache-2.0
 #![warn(clippy::pedantic)]
+#![cfg_attr(docsrs, feature(doc_cfg))]
 
 //! # Api Error
 //!
@@ -322,6 +323,7 @@ pub use ::http as __http;
 /// #[api_error(message = "Final")]
 /// ```
 #[cfg(feature = "derive")]
+#[cfg_attr(docsrs, doc(cfg(feature = "derive")))]
 pub use api_error_derive::ApiError;
 
 /// An error that can be returned by a service API.
@@ -376,6 +378,7 @@ pub trait ApiError: std::error::Error {
 
 /// Custom implementation for axum integration
 #[cfg(feature = "axum")]
+#[cfg_attr(docsrs, doc(cfg(feature = "axum")))]
 pub mod axum {
     use std::{borrow::Cow, sync::OnceLock};
 
